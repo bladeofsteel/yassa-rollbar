2146a6b9f1b15e59c6b337f55889593a4c02f7eb
Oleg Lobach <oleg@nanosemantics.ru>
2013-04-30 15:33:59 +0400

    Add option classes to classmap

--- autoload_classmap.php
+++ autoload_classmap.php
@@ -1,5 +1,7 @@
 <?php
 
 return array(
-    'Yassa\Rollbar\Module' => __DIR__ . '/Module.php',
+    'Yassa\Rollbar\Module'                       => __DIR__ . '/Module.php',
+    'Yassa\Rollbar\Options\ModuleOptionsFactory' => __DIR__ . '/src/Yassa/Rollbar/Options/ModuleOptionsFactory.php',
+    'Yassa\Rollbar\Options\ModuleOptions'        => __DIR__ . '/src/Yassa/Rollbar/Options/ModuleOptions.php',
 );
