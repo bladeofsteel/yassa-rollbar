eb5c1ba8ce7875b80918de00b3c37862c4298e7f
Oleg Lobach <oleg@lobach.info>
2013-05-04 02:14:26 +0400

    Bump version

--- tests/YassaRollbarTest/Factory/RollbarNotifierFactoryTest.php
+++ tests/YassaRollbarTest/Factory/RollbarNotifierFactoryTest.php
@@ -19,8 +19,8 @@
  * @copyright  Copyright (c) 2013 Oleg Lobach <oleg@lobach.info>
  * @license    Apache License V2 <http://www.apache.org/licenses/LICENSE-2.0.html>
  * @author     Oleg Lobach <oleg@lobach.info>
- * @version    0.1.3
- * @since      0.1.3
+ * @version    0.1.4
+ * @since      0.1.4
  */
 
 namespace YassaRollbarTest\Options;
