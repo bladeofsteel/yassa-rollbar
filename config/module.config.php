3f0e202149a8b6c0dffe6a028fdc0643e2415d17
Oleg Lobach <oleg@lobach.info>
2013-05-04 15:41:10 +0400

    lined the config rows

--- config/module.config.php
+++ config/module.config.php
@@ -3,7 +3,7 @@ return array(
     'service_manager' => array(
         'factories' => array(
             'Yassa\Rollbar\Options\ModuleOptions' => 'Yassa\Rollbar\Options\ModuleOptionsFactory',
-            'RollbarNotifier' => 'Yassa\Rollbar\Factory\RollbarNotifierFactory',
+            'RollbarNotifier'                     => 'Yassa\Rollbar\Factory\RollbarNotifierFactory',
             'Yassa\Rollbar\Log\Writer\Rollbar'    => 'Yassa\Rollbar\Factory\RollbarLogWriterFactory',
         ),
     ),
