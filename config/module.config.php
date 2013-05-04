48e12f72e76edf96306ee1b257c36b3bfd700403
Oleg Lobach <oleg@lobach.info>
2013-05-04 15:39:41 +0400

    Add factory for log writer

--- config/module.config.php
+++ config/module.config.php
@@ -4,6 +4,7 @@ return array(
         'factories' => array(
             'Yassa\Rollbar\Options\ModuleOptions' => 'Yassa\Rollbar\Options\ModuleOptionsFactory',
             'RollbarNotifier' => 'Yassa\Rollbar\Factory\RollbarNotifierFactory',
+            'Yassa\Rollbar\Log\Writer\Rollbar'    => 'Yassa\Rollbar\Factory\RollbarLogWriterFactory',
         ),
     ),
 );
