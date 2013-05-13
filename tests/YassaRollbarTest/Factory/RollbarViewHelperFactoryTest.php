602bd99a3e0b389a4db28e8111fb6d729e17e904
Oleg Lobach <oleg@nanosemantics.ru>
2013-05-13 18:43:06 +0400

    Add factory for helper and make helpr accessable for view

---
+++ tests/YassaRollbarTest/Factory/RollbarViewHelperFactoryTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Yassa Rollbar test case
+ *
+ * Copyright 2013 Oleg Lobach <oleg@lobach.info>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @copyright  Copyright (c) 2013 Oleg Lobach <oleg@lobach.info>
+ * @license    Apache License V2 <http://www.apache.org/licenses/LICENSE-2.0.html>
+ * @author     Oleg Lobach <oleg@lobach.info>
+ * @version    0.2.0
+ * @since      0.1.4
+ */
+
+namespace YassaRollbarTest\Factory;
+
+use YassaRollbarTest\TestCase;
+
+/**
+ * Test for RollbarViewHelperFactory class
+ *
+ * @package YassaRollbarTest\Factory
+ */
+class RollbarViewHelperFactoryTest extends TestCase
+{
+    public function testFactoryShouldReturnHelper()
+    {
+        $helper = $this->getServiceLocator()->get('ViewHelperManager')->get('rollbar');
+        $this->assertInstanceOf('\Yassa\Rollbar\View\Helper\Rollbar', $helper);
+    }
+}
