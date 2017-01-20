3d6f4a2e088f83c00b52e380013ea92e5b92e4bd
Ilya Kovalyov <ikovalyov85@gmail.com>
2017-01-20 16:12:16 +0100

    ( ! ) Fatal error: Class Yassa\Rollbar\Factory\RollbarNotifierFactory contains 1 abstract method and must therefore be declared abstract or implement the remaining methods (Zend\ServiceManager\Factory\FactoryInterface::__invoke) in /opt/webapp/dnsperf-admin-panel/vendor/prospect-one/yassa-rollbar/src/Yassa/Rollbar/Factory/RollbarNotifierFactory.php on line 37

--- src/Yassa/Rollbar/Options/ModuleOptionsFactory.php
+++ src/Yassa/Rollbar/Options/ModuleOptionsFactory.php
@@ -1,33 +1,9 @@
 <?php
-/**
- * Module options factory
- *
- * Copyright 2013 Oleg Lobach <oleg@lobach.info>
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- * @copyright  Copyright (c) 2013 Oleg Lobach <oleg@lobach.info>
- * @license    Apache License V2 <http://www.apache.org/licenses/LICENSE-2.0.html>
- * @author     Oleg Lobach <oleg@lobach.info>
- * @version    0.3.0
- * @since      0.1.3
- */
 
 namespace Yassa\Rollbar\Options;
 
 use Interop\Container\ContainerInterface;
 use Zend\ServiceManager\Factory\FactoryInterface;
-use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * Factory for ModuleOptions class
@@ -47,15 +23,5 @@ class ModuleOptionsFactory implements FactoryInterface
         $config = $container->get('Config');
 
         return new ModuleOptions(isset($config['yassa_rollbar']) ? $config['yassa_rollbar'] : array());
-    }
-
-    /**
-     * @param ServiceLocatorInterface $serviceLocator
-     *
-     * @return ModuleOptions
-     */
-    public function createService(ServiceLocatorInterface $serviceLocator)
-    {
-        return $this($serviceLocator, ModuleOptions::class, []);
     }
 }
